9a907d47178fae260c8f71cfde38b5b7af976494
genius <marinov727272@gmail.com>
2019-02-19 17:22:56 +0300

    csharp

---
+++ что то связанное с гитом/что то связанное с гитом/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace что_то_связанное_с_гитом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adfasfas");
+            Console.ReadLine();
+        }
+    }
+}
