6df4ca6a4c7369627b793595f88ae32d31dc32b9
genius <marinov727272@gmail.com>
2019-02-25 16:57:42 +0300

    задача

---
+++ программа 1/дз в гите по си шарпу двумерные массивы/дз в гите по си шарпу двумерные массивы/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз_в_гите_по_си_шарпу_двумерные_массивы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
