f9eb92e37644b06483c65508db9159b65ebed07c
genius <marinov727272@gmail.com>
2019-03-15 18:39:06 +0300

    дз 15.03.19 выполнено исключения

---
+++ Шарики/исключения НАЧАЛО/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace исключения_НАЧАЛО
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception();
+                Console.WriteLine();
+            }
+            catch
+            {
+
+                Console.WriteLine("УЧИ МАТЧАСТЬ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ проверка отр чисел/проверка отр чисел/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace проверка_отр_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = int.Parse(Console.ReadLine());
+                if (mass[i] < 0)
+                {
+                    try
+                    {
+                        throw new Exception();
+                    }
+                    catch
+                    {
+
+                        Console.WriteLine("ЭЭЭЭЭЭЭЭЭЭ введи плюс число");
+                        
+
+                    }
+                    mass[i] = int.Parse(Console.ReadLine());
+                }
+                
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
